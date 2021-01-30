cb478e7cb200171e86a3dc4491fa07c2c102cfa3
Willer Silva <willerrodrigo2006@hotmail.com>
2021-01-29 22:40:39 -0300

    feat(link component): Add style prop

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -4,12 +4,15 @@ import NextLink from 'next/link'
 type LinkProps = {
   children: React.ReactNode
   href: string
+  style: any
 }
 
-export default function Link({ children, href, ...props }: LinkProps) {
+export default function Link({ children, href, style, ...props }: LinkProps) {
   return (
     <NextLink href={href} passHref>
-      <a {...props}>{children}</a>
+      <a style={style} {...props}>
+        {children}
+      </a>
     </NextLink>
   )
 }
